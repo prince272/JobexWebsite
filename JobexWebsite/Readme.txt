f550ee24b1c40b02b951f9437e19ba6bd6f02a51
Prince Owusu <prince.owusu@hubtel.com>
2025-07-12 23:02:12 +0000

    Add project files.

---
+++ JobexWebsite/Readme.txt
@@ -0,0 +1,23 @@
+﻿
+https://www.gmail.com / https://my.freenom.com
+Username: freeeunimak@gmail.com
+Password: eunimak1234
+Password2: tulmtvpmjstkften
+Phone: +233 0561934865
+Date: 15 July 1999
+
+
+https://gearhost.com
+Username: freeeunimak@gmail.com
+Password: eunimak@1234
+
+https://my.freenom.com
+Domain:
+eunimakfashion.ml ACTIVE
+eunimakfashion.tk ACTIVE
+Registration Date:
+24/09/2018
+Expiry date:
+24/09/2019
+
+
---
+++ JobexWebsite/wwwroot/js/site.js
@@ -0,0 +1,72 @@
+﻿(function ($) {
+    'use strict';
+
+    // :: Preloader
+    //$(function () {
+    //    $(window).on('load', function () {
+    //        $('.preloader').delay(500).fadeOut('slow');
+    //        $('body').delay(500).css({ 'overflow': 'visible' });
+    //    });
+    //})
+
+    // :: Navbar Active Activation
+    $(function () {
+        $('#navbar').find('a').each(function () {
+            var oldUrl = $(this)[0].href.replace('.cshtml', '').replace('Pages/Home', '');
+            var newUrl = document.baseURI.replace('.cshtml', '').replace('Pages/Home', '');
+            if (newUrl.toLowerCase() == oldUrl.toLowerCase()) {
+                $(this).parent().addClass('active');
+            }
+            else {
+                $(this).parent().removeClass('active');
+            }
+        });
+    })
+
+    // :: Navbar Sticky Activation
+    $(function () {
+        $(window).on('scroll load resize', function (event) {
+            var navBarHeight = 37.6;
+            var scrollValue = $(window).scrollTop();
+
+            if (scrollValue > navBarHeight) {
+                $('#navbar').addClass('fixed-top');
+            }
+            else {
+                $('#navbar').removeClass('fixed-top');
+            }
+        });
+    })
+
+    // :: WOW Initialization
+    $(function () {
+        new WOW().init();
+    })
+
+    // :: Heroslider Initialization
+    $(function () {
+        var heroSlider = $('.heroslider .heroslider-inner');
+
+        heroSlider.camera({
+            loader: false,
+            navigation: true,
+            autoPlay: true,
+            time: 6000,
+            playPause: false,
+            pagination: false,
+            thumbnails: false,
+            overlayer: true,
+            hover: false,
+        });
+    })
+
+    // :: Tooltips Initialization
+    $(function () {
+        $('[data-toggle="tooltip"]').data('trigger', 'hover').tooltip();
+    })
+
+ 
+
+  
+
+})(jQuery);
